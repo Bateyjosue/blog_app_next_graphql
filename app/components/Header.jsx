ea0f0c2ee632b28e4258395de6172947acd7c4b2
Bateyjosue <josuebatey19@gmail.com>
2023-09-19 11:25:41 +0200

    add header component layout

---
+++ app/components/Header.jsx
@@ -0,0 +1,40 @@
+import React, {useContext} from 'react'
+import Link from 'next/link';
+
+const categories = [
+    {
+        name: 'React',
+        slug: 'react',
+    },
+    {
+        name: 'Web Development',
+        slug: 'web-dev',
+    }
+]
+
+const Header = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>
+        <div className="border-b w-full inline-block border-blue-400 py-8">
+            <div className='md:float-left block'>
+                <Link href="/">
+                    <span className='curson-pointer font-bold text-4xl text-white'>
+                        BlogApp
+                    </span>
+                </Link>
+            </div>
+            <nav className='hidden md:float-left md:contents'>
+                {categories.map(category => (
+                    <Link key={category.slug} href={`/category/${category.slug}`}>
+                        <span className='md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer'>
+                            {category.name}
+                        </span>
+                    </Link>
+                ))}
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Header
