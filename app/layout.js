7d2e60bd21626134c96d36dffb70a0678971370a
Bateyjosue <josuebatey19@gmail.com>
2023-09-19 11:26:35 +0200

    render the header component

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
-import './globals.css'
+// import './globals.css'
+import './globals.scss'
 import { Inter } from 'next/font/google'
+import { Header } from './components'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
